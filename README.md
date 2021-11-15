a007ba2c70f713436d73a4167ba8f2072df96186
Anthony Kuske <anthonykuske@gmail.com>
2021-11-15 13:07:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Removes some of the files that come with a default install of Laravel that are not neeed for a JSON API.
+
+Usage:
+
+```
+cd /path/to/laravel
+/path/to/clean-default-files.sh
+```
---
+++ clean-default-files.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+FILES=(
+    ".styleci.yml"
+    "app/Http/Controllers/Auth"
+    "database/factories/UserFactory.php"
+    "database/migrations/*.php"
+    "public/.htaccess"
+    "public/robots.txt"
+    "public/favicon.ico"
+    "public/css"
+    "public/js"
+    "public/web.config"
+    "resources/css"
+    "resources/js"
+    "resources/sass"
+    "resources/views"
+    "routes/web.php"
+    "server.php"
+    "tests/Feature/ExampleTest.php"
+    "tests/TestCase.php"
+    "tests/Unit/ExampleTest.php"
+    "webpack.mix.js"
+)
+for FILE in "${FILES[@]}"; do
+    # Just remove the .created from the directories
+    echo "${FILE}"
+    rm -rf ${FILE}
+done
